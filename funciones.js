5c0d6b35b5ff3996cec9707a44f7286adb1ea3e5
marto <marto9610@hotmail.com>
2018-04-26 20:54:58 -0500

    archivo js

---
+++ funciones.js
@@ -0,0 +1,20 @@
+alert("un mensaje de prueba");
+
+var numero = 3;
+var nombre = "abcd";
+var numero2 = 2;
+var resultado = 0;
+
+resultado =numero + numero2;
+
+alert(resultado); 
+
+var m = "global";
+function muestraMensaje(){
+    m = "local";
+    alert(m);
+}
+
+alert(m);
+muestraMensaje();
+alert(m)
